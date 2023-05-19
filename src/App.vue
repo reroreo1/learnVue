01c03c2b4389f38b3c9583d18f4667df01c519b3
Reroreo1 <86200459+skyjumper24@users.noreply.github.com>
2023-05-19 22:14:43 +0100

    css kind of working hahahah

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
 <div id="app">
     <h1>ToDo App</h1>
-    <form @submit.prevent="addTodo()">
-        <label>New ToDo </label>
-        <input v-model="newTodo" name="newTodo" autocomplete="off">
-        <button>Add ToDo</button>
+    <form :style="{'text-align':'center'}" @submit.prevent="addTodo()">
+        <label :style="{'text-align':'center'}" >New Tasks </label>
+        <input :style="{'text-align':'center'}" v-model="newTodo" name="newTodo" autocomplete="off">
+        <button>Add Tasks</button>
     </form>
     <h2>ToDo List</h2>
-    <ul>
+    <ul :style="{'text-align':'center'}">
       <li
         v-for="(todo, index) in todos"
         :key="index"
@@ -16,10 +16,10 @@
           :class="{ done: todo.done }"
           @click="doneTodo(todo)"
         >{{ todo.content }}</span>
-        <button @click="removeTodo(index)">Remove</button>
+        <button class="button" @click="removeTodo">Remove</button>
       </li>
     </ul>
-    <h4 v-if="todos.length === 0">Empty list.</h4>
+    <h4 :style="{'text-align':'center'}" v-if="todos.length === 0">Empty list.</h4>
 </div>
 </template>
 
@@ -74,7 +74,80 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Fjalla+One'); 
+
+body {
+  background-color: #ffab57;
+  font-family: 'Fjalla One', sans-serif;
+  text-transform: uppercase;
+  height: 95vh;
+  display: flex;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+
+button {
+	border-radius: 4px;
+	background-color: #9000ff;
+	border: none;
+	color: #fff;
+	text-align: center;
+	font-size: 32px;
+	padding: 16px;
+	width: 170px;
+	transition: all 0.5s;
+	cursor: pointer;
+	margin: 36px;
+	box-shadow: 0 10px 20px -8px rgba(0, 0, 0,.7);
+}
+  
+.button{
+cursor: pointer;
+display: inline-block;
+position: relative;
+transition: 0.5s;
+ }
+ 
+.button:after {
+content: '»';
+position: absolute;
+opacity: 0;  
+top: 14px;
+right: -20px;
+transition: 0.5s;
+ }
+ 
+.button:hover{
+padding-right: 24px;
+padding-left:8px;
+ }
+ 
+.button:hover:after {
+opacity: 1;
+right: 10px;
+ }
+
+#app {
+max-width: 600px;
+margin-left: auto;
+margin-right: auto;
+padding: 20px;
+}
+h1 {
+  font-weight: bold;
+  font-size: 28px;
+  text-align: center;
+}
+h2 {
+  font-weight: bold;
+  font-size: 24px;
+  text-align: center;
+  margin-top: 20px;
+}
+
 .done {
   text-decoration: line-through;
 }
-</style>// eslint-disable-next-line
+</style>
